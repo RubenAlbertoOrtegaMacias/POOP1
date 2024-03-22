27e059e7859dc891cd020a40f3656574a8eb6cce
RubenAlbertoOrtegaMacias <54441144+RubenAlbertoOrtegaMacias@users.noreply.github.com>
2024-03-22 00:16:59 -0600

    POOP1

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1;
+
+/**
+ *Programa que imprime un hola mundo
+ * @author RUBENO
+ */
+public class POOP1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+        * imprime la frase Hola Mundo desde POO grupo 1
+        */
+        System.out.println("Hola Mundo desde POO grupo 1");
+    }
+    
+}
+
